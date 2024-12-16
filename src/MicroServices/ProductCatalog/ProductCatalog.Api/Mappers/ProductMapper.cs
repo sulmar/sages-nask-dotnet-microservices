cb1c738b25b4a5081e49efe8aad84f51e520ae3a
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-12-16 16:59:27 +0100

    Dodanie ręcznego mapowania DTO

---
+++ src/MicroServices/ProductCatalog/ProductCatalog.Api/Mappers/ProductMapper.cs
@@ -0,0 +1,24 @@
+﻿using ProductCatalog.Api.DTOs;
+using ProductCatalog.Domain.Entities;
+
+namespace ProductCatalog.Api.Mappers;
+
+internal class ProductMapper
+{
+    public ProductDTO Map(Product product)
+    {
+        var dto = new ProductDTO
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Price = product.Price,
+        };
+
+        return dto;
+    }
+
+    public Product Map(ProductDTO dto)
+    {
+        throw new NotImplementedException();
+    }
+}
