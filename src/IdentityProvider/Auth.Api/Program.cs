2db535929cc790eca0e89d5176ffefc2d2d67993
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-12-18 15:30:39 +0100

    Dodanie Password Hasher

--- src/IdentityProvider/Auth.Api/Program.cs
+++ src/IdentityProvider/Auth.Api/Program.cs
@@ -1,6 +1,7 @@
 using Auth.Api.Abstractions;
 using Auth.Api.Infrastructure;
 using Auth.Api.Models;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddTransient<ITokenService, JwtTokenService>();
+builder.Services.AddTransient<IPasswordHasher<LoginRequest>, PasswordHasher<LoginRequest>>();
 
 var app = builder.Build();
 
@@ -23,13 +25,19 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 
-app.MapPost("api/login", (LoginRequest request, ITokenService tokenService, HttpContext context) =>
+app.MapPost("api/login", (LoginRequest request, ITokenService tokenService,
+    HttpContext context,
+    IPasswordHasher<LoginRequest> passwordHasher) =>
 {
-if (request.Username == "john" && request.Password == "123")
-{
-    var identity = new UserIdentity { Username = "john", FirstName = "John", LastName = "Smith", Email = "john@domain.com", Birthday = DateTime.Parse("2000-12-01") };
+    string hashedPassword = passwordHasher.HashPassword(request, "123");
 
-    var accessToken = tokenService.CreateAccessToken(identity);
+    var result = passwordHasher.VerifyHashedPassword(request, hashedPassword, request.Password);
+
+    if (request.Username == "john" && result == PasswordVerificationResult.Success)
+    {
+        var identity = new UserIdentity { Username = "john", FirstName = "John", LastName = "Smith", Email = "john@domain.com", Birthday = DateTime.Parse("2000-12-01") };
+
+        var accessToken = tokenService.CreateAccessToken(identity);
 
         context.Response.Cookies.Append("access-token", accessToken, new CookieOptions
         {
